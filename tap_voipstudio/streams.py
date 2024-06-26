757bf984c4e9e45fb953186e467da0b9d3e466cb
Afonso Diniz <afonso@rauva.com>
2024-06-26 09:53:03 +0100

    chore: main developments. Tested invoke to output.json

--- tap_voipstudio/streams.py
+++ tap_voipstudio/streams.py
@@ -15,58 +15,13 @@ else:
     import importlib_resources
 
 
-# TODO: Delete this is if not using json files for schema definition
 SCHEMAS_DIR = importlib_resources.files(__package__) / "schemas"
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
 
+class CDRS(VOIPStudioStream):
+    """Stream for CDRs endpoint"""
 
-class UsersStream(VOIPStudioStream):
-    """Define custom stream."""
+    name = "cdrs"
+    path = "/cdrs"
+    primary_keys = ["uri"]
 
-    name = "users"
-    path = "/users"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID",
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years",
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address",
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format",
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
-
-
-class GroupsStream(VOIPStudioStream):
-    """Define custom stream."""
-
-    name = "groups"
-    path = "/groups"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
-    ).to_dict()
+    schema_filepath = SCHEMAS_DIR / "cdrs.json"  # noqa: ERA001
--- tap_voipstudio/tap.py
+++ tap_voipstudio/tap.py
@@ -14,32 +14,20 @@ class TapVOIPStudio(Tap):
 
     name = "tap-voipstudio"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
+        th.Property(
+            "api_url",
+            th.StringType,
+            default="https://l7api.com/v1.2/voipstudio",
+            description="The url for the API service",
+        ),
         th.Property(
             "auth_token",
             th.StringType,
             required=True,
             secret=True,  # Flag config as protected.
             description="The token to authenticate against the API service",
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate",
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType,
-            description="The earliest record date to sync",
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service",
-        ),
+        )
     ).to_dict()
 
     def discover_streams(self) -> list[streams.VOIPStudioStream]:
@@ -49,8 +37,7 @@ class TapVOIPStudio(Tap):
             A list of discovered streams.
         """
         return [
-            streams.GroupsStream(self),
-            streams.UsersStream(self),
+            streams.CDRS(self)
         ]
 
 
